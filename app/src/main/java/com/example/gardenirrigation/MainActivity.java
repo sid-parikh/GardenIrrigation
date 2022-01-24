41c2f513ee9730a7cf62671310b78530f6bbf634
Sid Parikh <parikhsid16@gmail.com>
2022-01-23 23:02:04 -0500

    beginning to work with new navigation

--- app/src/main/java/com/example/gardenirrigation/MainActivity.java
+++ app/src/main/java/com/example/gardenirrigation/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.gardenirrigation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +11,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+}   
