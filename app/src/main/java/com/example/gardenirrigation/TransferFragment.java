90215ac43619d30f3f5fbdcf7777fc6d9a69fc24
Sid Parikh <parikhsid16@gmail.com>
2022-02-03 10:36:56 -0500

    Abstracted permissions to PermissionsFragment

--- app/src/main/java/com/example/gardenirrigation/TransferFragment.java
+++ app/src/main/java/com/example/gardenirrigation/TransferFragment.java
@@ -1,39 +1,30 @@
 package com.example.gardenirrigation;
 
 import static com.welie.blessed.WriteType.WITHOUT_RESPONSE;
-import static com.welie.blessed.WriteType.WITH_RESPONSE;
 
+import android.Manifest;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
-import android.Manifest;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.os.Handler;
 import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts.RequestMultiplePermissions;
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import java.util.UUID;
-
 import com.welie.blessed.BluetoothCentralManager;
+import com.welie.blessed.BluetoothCentralManagerCallback;
+import com.welie.blessed.BluetoothPeripheral;
 import com.welie.blessed.BluetoothPeripheralCallback;
-import com.welie.blessed.*;
+import com.welie.blessed.GattStatus;
+
+import java.util.UUID;
 
 
 /**
@@ -41,79 +32,77 @@ import com.welie.blessed.*;
  * Use the {@link TransferFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TransferFragment extends Fragment {
-    private Context mContext;
+public class TransferFragment extends PermissionsFragment {
 
-    UUID serviceUuid = UUID.fromString("91a40d83-3af0-4cb0-a959-97c0d4f74aeb");
+    // Argument keys
+    private static final String ARG_PARAM_MOISTURE =
+            "com.example.gardenirrigation.TransferFragment.param.moisture";
+    private static final String ARG_PARAM_SSID =
+            "com.example.gardenirrigation.TransferFragment.param.ssid";
+    private static final String ARG_PARAM_PASSWORD =
+            "com.example.gardenirrigation.TransferFragment.param.password";
 
-    private static final String[] PERMISSIONS_BLUETOOTH =
-            (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) ?
-            new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.BLUETOOTH_SCAN,
-                    Manifest.permission.BLUETOOTH_CONNECT,
-                    Manifest.permission.BLUETOOTH_ADVERTISE
-            } : new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.BLUETOOTH,
-                    Manifest.permission.BLUETOOTH_ADMIN,
-                    };
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM_MOISTURE = "com.example.gardenirrigation.TransferFragment.param.moisture";
-    private static final String ARG_PARAM_SSID = "com.example.gardenirrigation.TransferFragment.param.ssid";
-    private static final String ARG_PARAM_PASSWORD = "com.example.gardenirrigation.TransferFragment.param.password";
 
-    private String mParamMoisture;
+    // UUID for bluetooth communications
+    private static final UUID SERVICE_UUID =
+            UUID.fromString("91a40d83-3af0-4cb0-a959-97c0d4f74aeb");
+    // Define callbacks for when we connect to the bluetooth device
+    private final BluetoothPeripheralCallback peripheralCallback =
+            new BluetoothPeripheralCallback() {
+                @Override
+                public void onCharacteristicUpdate(
+                        @NonNull BluetoothPeripheral peripheral,
+                        @NonNull byte[] value,
+                        @NonNull BluetoothGattCharacteristic characteristic,
+                        @NonNull GattStatus status) {
+                    super.onCharacteristicUpdate(peripheral, value, characteristic, status);
+                    // Callback for read operations. Results can be accessed here.
+                }
+
+                @Override
+                public void onCharacteristicWrite(
+                        @NonNull BluetoothPeripheral peripheral,
+                        @NonNull byte[] value,
+                        @NonNull BluetoothGattCharacteristic characteristic,
+                        @NonNull GattStatus status) {
+                    super.onCharacteristicWrite(peripheral, value, characteristic, status);
+                    // Callback for write operations.
+                }
+
+                @Override
+                public void onServicesDiscovered(@NonNull BluetoothPeripheral peripheral) {
+                    super.onServicesDiscovered(peripheral);
+                    BluetoothGattService scoutingService = peripheral.getServices().get(0);
+
+                    BluetoothGattCharacteristic dataCharacteristic =
+                            scoutingService.getCharacteristics().get(0);
+
+                    byte[] dataToBeWritten = {0};
+
+                    peripheral.writeCharacteristic(dataCharacteristic, dataToBeWritten,
+                            WITHOUT_RESPONSE);
+                }
+            };
+    // Actual arguments that we receive are stored here
+    private int mParamMoisture;
     private String mParamSsid;
     private String mParamPassword;
 
-    private final ActivityResultLauncher<String[]> requestBluetoothPermissionsLauncher =
-            registerForActivityResult(
-                    new RequestMultiplePermissions(), (isGranted) -> {
-                        if (!isGranted.containsValue(false)) {
-                            scan(mContext);
-                        }
-                    }
-            );
-    private final BluetoothCentralManagerCallback bluetoothCentralManagerCallback = new BluetoothCentralManagerCallback() {
-        @Override
-        public void onDiscoveredPeripheral(
-                @NonNull BluetoothPeripheral peripheral, @NonNull ScanResult scanResult){
-            central.stopScan();
-            central.connectPeripheral(peripheral, peripheralCallback);
-        }
-    };
-    private final BluetoothPeripheralCallback peripheralCallback = new BluetoothPeripheralCallback() {
-        @Override
-        public void onCharacteristicUpdate(
-                @NonNull BluetoothPeripheral peripheral,
-                @NonNull byte[] value,
-                @NonNull BluetoothGattCharacteristic characteristic, @NonNull GattStatus status) {
-            super.onCharacteristicUpdate(peripheral, value, characteristic, status);
-            // Callback for read operations. Results can be accessed here.
-        }
-        @Override
-        public void onCharacteristicWrite(
-                @NonNull BluetoothPeripheral peripheral,
-                @NonNull byte[] value,
-                @NonNull BluetoothGattCharacteristic characteristic, @NonNull GattStatus status) {
-            super.onCharacteristicWrite(peripheral, value, characteristic, status);
-            // Callback for write operations.
-        }
+    // Store context for use throughout the fragment
+    private Context mContext;
+    private BluetoothCentralManager central;
 
-        @Override
-        public void onServicesDiscovered(@NonNull BluetoothPeripheral peripheral) {
-            super.onServicesDiscovered(peripheral);
-            BluetoothGattService scoutingService = peripheral.getServices().get(0);
+    // Define callbacks for when we connect to the bluetooth device
+    private final BluetoothCentralManagerCallback bluetoothCentralManagerCallback =
+            new BluetoothCentralManagerCallback() {
+                @Override
+                public void onDiscoveredPeripheral(
+                        @NonNull BluetoothPeripheral peripheral, @NonNull ScanResult scanResult) {
+                    central.stopScan();
+                    central.connectPeripheral(peripheral, peripheralCallback);
+                }
+            };
 
-            BluetoothGattCharacteristic dataCharacteristic = scoutingService.getCharacteristics().get(0);
-
-            byte[] dataToBeWritten = {0};
-
-            peripheral.writeCharacteristic(dataCharacteristic, dataToBeWritten, WITHOUT_RESPONSE);
-        }
-    };
 
     public TransferFragment() {
         // Required empty public constructor
@@ -124,85 +113,38 @@ public class TransferFragment extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param moistureLevel The moisture level to be transferred.
-     * @param wifiSsid The ssid of the device to be connected to.
-     * @param wifiPassword The password of the device to be connected to.
+     * @param wifiSsid      The ssid of the device to be connected to.
+     * @param wifiPassword  The password of the device to be connected to.
      * @return A new instance of fragment TransferFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static TransferFragment newInstance(String moistureLevel, String wifiSsid, String wifiPassword) {
+    public static TransferFragment newInstance(int moistureLevel, String wifiSsid, String wifiPassword) {
         TransferFragment fragment = new TransferFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM_MOISTURE, moistureLevel);
+        args.putInt(ARG_PARAM_MOISTURE, moistureLevel);
         args.putString(ARG_PARAM_SSID, wifiSsid);
         args.putString(ARG_PARAM_PASSWORD, wifiPassword);
         fragment.setArguments(args);
         return fragment;
     }
 
-    BluetoothCentralManager central = new BluetoothCentralManager(mContext.getApplicationContext(),
-            bluetoothCentralManagerCallback, new Handler(Looper.getMainLooper()));
-
-
-    /**
-     * Checks if the app has permission to access Bluetooth. If not, checks if a rationale should be
-     * displayed. If so, displays a dialog to the user and then requests the permission. Then, if the
-     * user has granted the permission, runs {@link #scan(Context)}.
-     *
-     * @param context
-     */
-    private void checkBluetoothPermissionsAndScan(@NonNull Context context) {
-        // Ensure all permissions are granted
-        boolean areAllPermissionsGranted = true;
-        for (String permission : PERMISSIONS_BLUETOOTH) {
-            areAllPermissionsGranted &= ContextCompat.checkSelfPermission(context, permission) ==
-                                        PackageManager.PERMISSION_GRANTED;
-        }
-
-        if (areAllPermissionsGranted) {
-            scan(context);
-        } else {
-            // Check if any rationales are needed
-            boolean shouldShowRationale = false;
-            for (String permission : PERMISSIONS_BLUETOOTH) {
-                shouldShowRationale |= shouldShowRequestPermissionRationale(permission);
-            }
-
-            // Show the rationale if needed
-            if (shouldShowRationale) {
-                showBtPermExplanation();
-            } else {
-                requestBluetoothPermissionsLauncher.launch(PERMISSIONS_BLUETOOTH);
-            }
-        }
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
     }
 
-    private void showBtPermExplanation() {
-        // Build an alert dialog to explain why we need Bluetooth permission
-        // If the user grants permission, request them from the system
-        new AlertDialog.Builder(getActivity())
-                .setMessage(R.string.bluetooth_permission_explanation)
-                .setTitle(R.string.bluetooth_permission_explanation_title)
-                .setPositiveButton(R.string.proceed,
-                        (dialog, which) -> {
-                            requestBluetoothPermissionsLauncher.launch(
-                                    PERMISSIONS_BLUETOOTH);
-                        })
-                .setNegativeButton(R.string.cancel,
-                        (dialog, which) -> {
-                            dialog.dismiss();
-                        })
-                .create()
-                .show();
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParamMoisture = getArguments().getString(ARG_PARAM_MOISTURE);
+            mParamMoisture = getArguments().getInt(ARG_PARAM_MOISTURE);
             mParamSsid = getArguments().getString(ARG_PARAM_SSID);
             mParamPassword = getArguments().getString(ARG_PARAM_PASSWORD);
         }
+
+        central = new BluetoothCentralManager(mContext.getApplicationContext(),
+                bluetoothCentralManagerCallback, new Handler(Looper.getMainLooper()));
     }
 
     @Override
@@ -211,5 +153,27 @@ public class TransferFragment extends Fragment {
             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_transfer, container, false);
+    }
+
+
+    @Override
+    protected String[] getPermissionsToRequest() {
+        // Permissions to ask for, dependent on version
+        return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) ?
+               new String[]{
+                       Manifest.permission.ACCESS_FINE_LOCATION,
+                       Manifest.permission.BLUETOOTH_SCAN,
+                       Manifest.permission.BLUETOOTH_CONNECT,
+                       Manifest.permission.BLUETOOTH_ADVERTISE
+               } : new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN,
+                };
+    }
+
+    @Override
+    protected void onPermissionsGranted() {
+        // scan(mContext);
     }
 }
